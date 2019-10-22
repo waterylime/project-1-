4f92ab646b64987541180381a59e15a2d5eacd4a
kwlahr <kwlahr@gmail.com>
2019-10-21 20:11:56 -0600

    added styling to style.css and script.js

--- script.js
+++ script.js
@@ -15,36 +15,38 @@
                 $("#result").empty();
 
                 var bookDiv = $("<div>").addClass("book");
+                var yourResult = $(`<h2 class="new">Your Search</h2>`);
                 var pOne = $("<p>").text("Book: " + response.items[0].volumeInfo.title);
                 var pTwo = $("<p>").text("Author: " + response.items[0].volumeInfo.authors[0]);
                 pTwo.attr("id", "author");
                 var pThree = $("<img src=" + response.items[0].volumeInfo.imageLinks.thumbnail + ">");
                 var pFour = $("<p>").text("Synopsis: " + response.items[0].volumeInfo.description);
-                bookDiv.append(pOne, pTwo, pThree, pFour);
+                bookDiv.append(yourResult, pOne, pTwo, pThree, pFour);
                 $("#result").prepend(bookDiv);
-
+                
                 otherBooks(author);
             })      
-
-    })
-
-    function otherBooks(author) {
-    
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://www.googleapis.com/books/v1/volumes?q=" + author + "&key=AIzaSyApn7ZVS6vtzHfrwGpr058OiKyAkrZ_U6o";
-    
+            
+        })
+        
+        function otherBooks(author) {
+            
+            var queryURL = "https://cors-anywhere.herokuapp.com/https://www.googleapis.com/books/v1/volumes?q=" + author + "&key=AIzaSyApn7ZVS6vtzHfrwGpr058OiKyAkrZ_U6o";
+            
         $.ajax({
             url : queryURL,
             method : "GET"
         }).then(function(response){
             $(".results").empty();
-
+            var newReads = $(`<h2 class="new">Your Recommended Reads</h2>`);
+            
             for (var i = 1; i < response.items.length; i++) {
                 var bookLoop = response.items[i].volumeInfo;
                 var booksDiv = $("<div>").addClass("card");
                 var bookImage = $("<img src=" + response.items[i].volumeInfo.imageLinks.thumbnail + ">");
                 booksDiv.attr("data-book", bookImage);
                 booksDiv.append(bookImage);
-                $(".results").prepend(booksDiv);
+                $(".results").prepend(newReads, booksDiv);
                 
             }
         });
